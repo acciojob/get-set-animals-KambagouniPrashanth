063c563f978deec3d835506542546f55d4c434e2
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-08-08 06:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Animal {}
+class Animal {
+    species;
+    constructor(species){
+        this.species=species;
+    }
+    get makeSound(){
+        return `${this.species}sound the animal makes` ; 
+     }
 
-class Dog extends Animal {}
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+    constructor(species){
+        super(species)
+
+
+    }
+    purr(){
+        console.log("purr")
+    }
+}
+
+class Cat extends Animal {
+    constructor(species){
+        super(species)
+
+
+    }
+    bark(){
+        console.log("woof")
+    }
+
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
